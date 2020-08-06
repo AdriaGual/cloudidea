8d322571fd4bc5636bc5f8616d315d6f37ffa766
u1933571@campus.udg.edu <luagairda1>
2020-08-06 20:12:47 +0200

    Translation done

--- src/pages/moderate/ModerateUsers.vue
+++ src/pages/moderate/ModerateUsers.vue
@@ -13,8 +13,8 @@
     <div class="row q-pt-md">
       <div class="col-3" v-if="this.$q.platform.is.desktop && $q.screen.gt.sm"></div>
       <div class="col" style="border-radius: 0.5em">
-        <div v-for="(user, key) in users" :key="key">
-          <div v-if="key!==userDetails.userId">
+        <div v-for="(user, key) in orderedUsers" :key="key">
+          <div v-if="user.key!==userDetails.userId">
             <q-item clickable v-ripple>
               <q-item-section side>
                 <q-avatar rounded size="4em">
@@ -30,12 +30,13 @@
               <q-item-section side>
                 <div class="row q-gutter-sm" v-if="!user.moderator">
                   <div class="col">
-                    <q-btn color="green" outline size="md" @click="promoteToModerator(key)">
+                    <q-btn color="green" outline size="md"
+                           @click="openPromoteToModerator(user,user.key)">
                       <q-icon name="fas fa-crown" size="xs"/>
                     </q-btn>
                   </div>
                   <div class="col">
-                    <q-btn color="red" size="md" outline @click="deleteUser(key)">
+                    <q-btn color="red" size="md" outline @click="openDeleteUser(user,user.key)">
                       <q-icon name="delete"/>
                     </q-btn>
                   </div>
@@ -51,6 +52,40 @@
 
     </div>
     <div class="q-pb-xl"></div>
+    <q-dialog v-model="showPromoteToModerator" v-if="selectedUser">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Are you sure to make {{selectedUser.name}} a <b>moderator</b>?
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="YES" color="green-6" v-close-popup
+                 @click="promoteToModerator(selectedUser.key)"/>
+          <q-btn flat label="CANCEL" color="primary" v-close-popup/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <q-dialog v-model="showDeleteUser" v-if="selectedUser">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Are you sure to remove {{selectedUser.name}}'s account?
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="YES" color="red-10" v-close-popup
+                 @click="deleteUser(selectedUser.key)"/>
+          <q-btn flat label="CANCEL" color="primary" v-close-popup/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -60,13 +95,27 @@
   export default {
     data() {
       return {
-        url: 'https://placeimg.com/500/300/nature'
+        showPromoteToModerator: false,
+        selectedUser: null,
+        showDeleteUser: false,
+        orderedUsers: []
       }
     },
     methods: {
-      ...mapActions('store', ['firebaseGetUsers', 'firebaseUpdateUser', 'firebaseDeleteUser']),
+      ...mapActions('store',
+        ['firebaseGetUsers', 'firebaseUpdateUser', 'firebaseDeleteUser', 'clearUsers']),
       goToPage(route) {
         this.$router.push(route)
+      },
+      openPromoteToModerator(user, key) {
+        this.selectedUser = user;
+        this.selectedUser.key = key;
+        this.showPromoteToModerator = true
+      },
+      openDeleteUser(user, key) {
+        this.selectedUser = user;
+        this.selectedUser.key = key;
+        this.showDeleteUser = true
       },
       promoteToModerator(key) {
         this.firebaseUpdateUser({
@@ -80,6 +129,26 @@
     },
     computed: {
       ...mapState('store', ['users', 'userDetails']),
+    },
+    watch: {
+      users: function (val) {
+        let keys = Object.keys(val);
+        keys.forEach(key => {
+          let item = this.users[key];
+          item.key = key
+          this.users[key].key = key
+          this.orderedUsers.push(this.users[key])
+        })
+        this.orderedUsers = this.orderedUsers.filter((a, b) => this.orderedUsers.indexOf(
+          a) === b)
+
+        this.orderedUsers.sort((a, b) => b.cp - a.cp);
+        console.log(this.orderedUsers)
+      }
+    },
+    created() {
+      this.clearUsers()
+      this.firebaseGetUsers()
     }
   };
 </script>
